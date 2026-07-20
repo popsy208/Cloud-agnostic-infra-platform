8bd11371673fefb219419a3a643a24ee58797222
Ibpopsy <ibrahimpopoola153@gmail.com>
2026-07-20 23:05:09 +0100

    feat(network): expose VPC route table outputs

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -12,3 +12,28 @@ output "internet_gateway_id" {
   value       = aws_internet_gateway.main.id
 
 }
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
+
+output "public_route_table_id" {
+
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+
+}
+
+
+output "private_route_table_id" {
+
+  description = "ID of the private route table"
+  value       = aws_route_table.private.id
+
+}
